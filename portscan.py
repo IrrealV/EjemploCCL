5102624f867d5219a1c1ea577f8e4e462e6ddf05
IrrealV <victor_heras@DESKTOP-0SQNEN6>
2023-10-02 20:19:55 +0200

    escaneo de puerto modificado

--- portscan.py
+++ portscan.py
@@ -1,39 +1,30 @@
 import socket
 
-# Dirección IP que deseas escanear
-target_ip = "127.0.0.1"
+def scan_ports(target_ip, start_port, end_port):
+    open_ports = []
 
-# Rango de puertos que deseas escanear (ejemplo: del puerto 1 al 1024)
-start_port = 1
-end_port = 1024
+    for port in range(start_port, end_port + 1):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(1)
+                s.connect((target_ip, port))
+                open_ports.append(port)
+        except (socket.timeout, ConnectionRefusedError):
+            pass
 
-def scan_port(ip, port):
-    try:
-        # Crear un objeto de socket TCP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)  # Establecer un tiempo de espera para la conexión
+    return open_ports
 
-        # Intentar conectar al puerto en la dirección IP
-        result = sock.connect_ex((ip, port))
+def main():
+    target_ip = input("Enter the target IP address: ")
+    start_port = int(input("Enter the starting port: "))
+    end_port = int(input("Enter the ending port: "))
 
-        # Comprobar si la conexión tuvo éxito
-        if result == 0:
-            print(f"Puerto {port} abierto")
-        else:
-            print(f"Puerto {port} cerrado")
+    open_ports = scan_ports(target_ip, start_port, end_port)
 
-        # Cerrar el socket
-        sock.close()
-    
-    except KeyboardInterrupt:
-        print("Escaneo interrumpido.")
-        exit()
-    
-    except socket.error:
-        print("No se pudo conectar al servidor.")
-        exit()
+    if open_ports:
+        print("Open ports on {}: {}".format(target_ip, ", ".join(map(str, open_ports))))
+    else:
+        print("No open ports found on {} in the specified range.".format(target_ip))
 
-# Escanear los puertos en el rango especificado
-for port in range(start_port, end_port + 1):
-    scan_port(target_ip, port)
-
+if __name__ == "__main__":
+    main()
